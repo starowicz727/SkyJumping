113ace455e47e3a333f4699bbbc362c9a60355cf
starowicz727 <starowicz727@gmail.com>
2024-02-22 00:39:02 +0100

    door opening

---
+++ Assets/DoorManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    private bool canBeOpen = true;
+    public GameObject blockingPlane;
+    private float timer = 5;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (canBeOpen)
+            {
+                if (timer>0)
+                {
+                    blockingPlane.SetActive(false);
+                    this.transform.Rotate( 0, 0, -20 * Time.deltaTime);
+                    timer -= Time.deltaTime;
+                }
+            }
+        }
+        
+    }
+}
--- Assets/KeysManager.cs
+++ Assets/KeysManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class KeysManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject door;
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if(collision.gameObject.tag == "Player")
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
