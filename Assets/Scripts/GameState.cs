073e06429e40798ec395b4aa68602a8dafaab320
starowicz727 <starowicz727@gmail.com>
2024-02-22 23:14:04 +0100

    post processing

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour
 {
     public static GameObject currentCheckpoint;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
 }
